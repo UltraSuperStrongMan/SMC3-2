e9e7be921ef522ce5faed82f846e796f55bd31f7
Administrator <Administrator@501-S04>
2022-04-18 09:54:24 +0900

    0411 슈퍼 솔져 프로젝트 2차 실험

---
+++ 0418/src/scan.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class scan {
+
+	public static void main(String[] args) {
+		Scanner SC = new Scanner(System.in);
+		int a = SC.nextInt();
+		int sum = 0;
+		for (int i = 0; i <= a; i++) {
+			if (i % 4 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println("1~" + a + "까지 4의 배수의 합은 " + sum + "입니다.");
+		SC.close();
+	}
+
+}
